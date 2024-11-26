9f1063a1d6227e3ddf430a2f5abc6b36e392c257
NeyleCM <neylecmorantes@gmail.com>
2024-11-26 21:25:44 +0100

    models

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,4 +1,4 @@
-const Product = require("../models/ProductModel");
+const Product = require("../models/productModel");
 
 exports.getAllProducts = async (req, res) => {
   const products = await Product.find();
