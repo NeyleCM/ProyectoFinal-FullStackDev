1cba94bd23088ebb3a84cad829c15cdd4719d9a5
NeyleCM <neylecmorantes@gmail.com>
2024-12-06 18:46:19 +0100

    firebase-register

--- config/firebase.js
+++ config/firebase.js
@@ -7,9 +7,11 @@ const serviceAccount = {
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 };
 
+if (!admin.apps.length) {
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+}
 
 module.exports = admin;
 /*
