49680314459963002877c827090450f569a5d389
NeyleCM <neylecmorantes@gmail.com>
2024-12-07 09:08:12 +0100

    cambios en verifyToken para register

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,3 +1,24 @@
+const admin = require("firebase-admin");
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.startsWith("Bearer ") ? authHeader.split(" ")[1] : null;
+  
+  if (!token) return res.status(403).send("Token required");
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.uid = decodedToken.uid; // Guardar el UID en la solicitud
+    next();
+  } catch (error) {
+    console.error("Token verification failed:", error);
+    res.status(401).send("Invalid token");
+  }
+};
+
+module.exports = verifyToken;
+
+/*
 const admin = require("../config/firebase"); 
 
 const verifyToken = async (req, res, next) => {
@@ -19,44 +40,4 @@ const verifyToken = async (req, res, next) => {
 
 module.exports = verifyToken;
 
-/*
-const admin = require('../config/firebase');
-
-const verifyToken = async (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1]; // Lee el token desde los headers
-  if (!token) {
-    return res.status(401).json({ error: 'Token no proporcionado' });
-  }
-
-  try {
-    const decodedToken = await admin.auth().verifyIdToken(token);
-    req.user = decodedToken; // Almacena el usuario decodificado
-    next();
-  } catch (error) {
-    res.status(401).json({ error: 'Token inválido o expirado' });
-  }
-};
-
-module.exports = verifyToken;
-*/
-
-/*
-const admin = require('../config/firebase');
-
-const verifyToken = async (req, res, next) => {
-  const token = req.headers['authorization']; 
-  if (!token) {
-    return res.status(403).json({ error: "No token provided" });
-  }
-
-  try {
-    const decodedToken = await admin.auth().verifyIdToken(token);
-    req.user = decodedToken; 
-    next(); 
-  } catch (error) {
-    return res.status(401).json({ error: "Invalid or expired token" });
-  }
-};
-
-module.exports = verifyToken;
 */
